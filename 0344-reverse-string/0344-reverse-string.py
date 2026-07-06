2a8a86b769da1d5f24419b34a3882d2feba6cca6
Pavitar <aethxinfra@gmail.com>
2026-07-06 22:57:54 +0530

    Time: 1 ms (57.33%), Space: 23.6 MB (18.31%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -3,21 +3,13 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        left = 0
-        right = len(s) - 1
-    
-    # Keep swapping as long as the left pointer hasn't crossed the right pointer
-        while left < right:
-        # Step 1: Temporarily hold the left letter so we don't lose it
+        left = 0 
+        right = len(s) -1 
+
+        while left < right :
             temp = s[left]
-        
-        # Step 2: Overwrite the left letter with the right letter
             s[left] = s[right]
-        
-        # Step 3: Put our temporarily held letter into the right spot
             s[right] = temp
-        
-        # Step 4: Move both pointers one step closer to the middle
-            left = left + 1
-            right = right - 1
-        
+
+            left = left+1
+            right = right-1
