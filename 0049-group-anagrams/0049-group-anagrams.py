e8e439c5a8875fd16a6b354652aaae09c9d54786
Pavitar <aethxinfra@gmail.com>
2026-07-12 23:16:21 +0530

    Time: 13 ms (50.33%), Space: 21.9 MB (79.3%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -2,8 +2,9 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         dictionary = {}
         for s in strs:
-            tag = "".join(sorted(s))
+            tag = ''.join(sorted(s))
             if tag not in dictionary:
                 dictionary[tag] = []
             dictionary[tag].append(s)
         return list(dictionary.values())
+        
