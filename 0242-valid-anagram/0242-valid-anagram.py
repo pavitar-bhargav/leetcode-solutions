15da00256805cc56dec86a664c1ac5d220c7600c
Pavitar <aethxinfra@gmail.com>
2026-07-12 23:01:38 +0530

    Time: 11 ms (77.15%), Space: 19.4 MB (45.3%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,3 +1,18 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+        if len(s) != len(t):
+            return False
+        oxford = {}
+        for letter in s :
+            if letter in oxford:
+                oxford[letter] += 1
+            else:
+                oxford[letter] = 1
+        for letter in t:
+            if letter not in oxford or oxford[letter] == 0:
+                return False
+            else:
+                oxford[letter] -= 1
+        return True
+
+        
