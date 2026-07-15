47abc453f1a9b62d2688fadc10e8982fd7d44e53
Pavitar <aethxinfra@gmail.com>
2026-07-15 22:42:46 +0530

    Time: 3 ms (89.57%), Space: 22.6 MB (99.25%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -2,10 +2,9 @@ class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         ox = {}
         for num in nums:
-            
             if num in ox:
                 ox[num] += 1
             else:
-                ox[num] = 1
+                ox[num] =1
         sorted_ox = sorted(ox, key = lambda num : ox[num], reverse = True)
         return sorted_ox[:k]
